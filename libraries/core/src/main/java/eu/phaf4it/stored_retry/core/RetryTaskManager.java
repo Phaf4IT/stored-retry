a77eb6f192b19466d89e94746048c197fc3e93ae
rafaelaphaf <rafaelabreed@gmail.com>
2024-08-17 21:30:02 +0200

    feat: some validation / warning

--- libraries/core/src/main/java/eu/phaf4it/stored_retry/core/RetryTaskManager.java
+++ libraries/core/src/main/java/eu/phaf4it/stored_retry/core/RetryTaskManager.java
@@ -1,12 +1,17 @@
 package eu.phaf4it.stored_retry.core;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.time.OffsetDateTime;
+import java.util.Arrays;
 import java.util.List;
 
 public class RetryTaskManager implements TaskManager {
     private final RetryTaskActionRepository retryTaskActionRepository;
     private final RetryRecurringJobFactory retryRecurringJobFactory;
     private final InstanceRepository instanceRepository;
+    private final static Logger LOG = LoggerFactory.getLogger(RetryTaskManager.class);
 
     public RetryTaskManager(
             RetryTaskActionRepository retryTaskActionRepository,
@@ -21,8 +26,25 @@ public class RetryTaskManager implements TaskManager {
     public RecurringRetryJob registerTask(
             RetryTask retryTask,
             Object originalInstance) {
+        validateRetryMethodWithClass(retryTask);
         instanceRepository.saveCallableClass(retryTask, originalInstance);
         return retryRecurringJobFactory.createRetryJob(retryTask);
+    }
+
+    public void validateRetryMethodWithClass(RetryTask retryTask) {
+        Arrays.stream(retryTask
+                        .task()
+                        .theClass()
+                        .getMethods())
+                .filter(method -> method.getName().equals(retryTask.retryMethod()))
+                .filter(method -> Arrays.equals(method.getParameterTypes(), retryTask.task().nonGenericParameters()
+                        .toArray(new Class<?>[0])))
+                .findAny()
+                .ifPresentOrElse(method -> {
+                }, () -> LOG.error("[Retry] Could not find retry method {} with parameters {} on class {}",
+                        retryTask.retryMethod(),
+                        retryTask.task().parameters(),
+                        retryTask.task().theClass()));
     }
 
     @Override
